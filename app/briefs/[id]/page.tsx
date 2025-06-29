8cf1a6a07c45ae4114263e6a99e59f2fbdb513d6
surepeps <gatukurh1@gmail.com>
2025-06-29 21:26:00 +0000

    Replace ConfirmationModal import with useConfirmation in brief detail page
    cgen-3ad9c472ddd3459b89738de3dfc3a9f1

--- app/briefs/[id]/page.tsx
+++ app/briefs/[id]/page.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ConfirmationModal } from "@/components/modals/ConfirmationModal";
+import { useConfirmation } from "@/contexts/ConfirmationContext";
 import {
   ArrowLeft,
   Edit,
