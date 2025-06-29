788e48fde9c92962a1a5f2ae776394b3ee2e15be
surepeps <gatukurh1@gmail.com>
2025-06-29 07:44:08 +0000

    view_path
    cgen-d3d41a02640c4a9f93be59f543e3b27b

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/mac/Downloads/KB-admin-FE/app/layout.tsx
+// File: /app/code/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
