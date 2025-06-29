af7c717e62e63bc8abc0e6a59b9f98c554156717
surepeps <gatukurh1@gmail.com>
2025-06-29 14:37:17 +0000

    Create view single brief page
    cgen-55866994f91c4c2b9d4729213056a5ad

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("2c19dd50dafe84ec")
+/******/ 		__webpack_require__.h = () => ("ede359195bfad632")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
