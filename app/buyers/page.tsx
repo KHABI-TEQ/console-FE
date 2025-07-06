54af19309cb4fc6ee1de466609402a878c1431a5
surepeps <gatukurh1@gmail.com>
2025-07-06 20:42:49 +0000

    Show preloader overlay when refreshing buyers data
    cgen-fcdb8a2cbbd141b5855152efd6fa15d1

--- app/buyers/page.tsx
+++ app/buyers/page.tsx
@@ -250,7 +250,7 @@ function BuyersContent() {
             </CardTitle>
           </CardHeader>
           <CardContent className="p-0">
-            {isLoading ? (
+            {isLoading || isRefreshing ? (
               <div className="relative">
                 <div className="absolute inset-0 bg-white/50 z-10 flex items-center justify-center">
                   <div className="w-8 h-8 border-2 border-blue-200 border-t-blue-600 rounded-full animate-spin"></div>
