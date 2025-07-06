296242b3c563f7ce0b8433c69b0843914df57f26
surepeps <gatukurh1@gmail.com>
2025-07-06 10:56:57 +0000

    Update formatCurrency to ensure Naira symbol is used consistently
    cgen-a1a49802332c429996a3c4cd86e6a291

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,5 +9,6 @@ export function formatCurrency(amount: number): string {
   return new Intl.NumberFormat("en-NG", {
     style: "currency",
     currency: "NGN",
+    minimumFractionDigits: 0,
   }).format(amount);
 }
